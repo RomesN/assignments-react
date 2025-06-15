955dd709dbc61f47267fbb75cc22beaccb833f04
Roman Nemeth <romannemeth1@gmail.com>
2025-06-16 00:24:20 +0200

    feat(SB3): Add Hovered story to demonstrate button visibility on hover

--- client/src/components/stories/ListItem.stories.tsx
+++ client/src/components/stories/ListItem.stories.tsx
@@ -9,6 +9,14 @@ const meta = {
         onItemDelete: { action: "removed" },
         onItemLabelEdit: { action: "edited" },
     },
+    parameters: {
+        docs: {
+            description: {
+                component:
+                    'This component show buttons when the user hovers over it. Check the "Hovered" story to see this state directly.',
+            },
+        },
+    },
 } as Meta<typeof ListItem>;
 export default meta;
 type Story = StoryObj<typeof ListItem>;
@@ -21,5 +29,17 @@ export const Done: Story = {
     args: {
         ...ToDo.args,
         isDone: true,
+    },
+};
+
+export const Hovered: Story = {
+    args: {
+        label: "Hovered item",
+    },
+    play: async ({ canvasElement }) => {
+        const buttons = canvasElement.querySelectorAll("button");
+        buttons.forEach((button) => {
+            button.style.visibility = "visible";
+        });
     },
 };
